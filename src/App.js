82f27fba65bfc43bb4a746f7a63aba96d28d6383
ChanX21 <47290661+ChanX21@users.noreply.github.com>
2021-06-02 15:18:07 +0530

    More Changes updated

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import React, { Component } from 'react';
 
 
 var Annex = "Project";
+var Contract = require('web3-eth-contract');
 
 
 
@@ -33,18 +34,34 @@ class App extends Component {
       window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
     }
   }
-
+  
   async loadBlockchainData() {
     const web3 = window.web3
     // Load account
     const accounts = await web3.eth.getAccounts()
+    console.log(accounts)
     this.setState({ account: accounts[0] })
     // Network ID
     const networkId = await web3.eth.net.getId()
+    
     const networkData = basic_Crowdfund.networks[networkId]
-    
-    
+    this.setState({ NetID : networkId})
+    const basicCrowdfund = new web3.eth.Contract(basic_Crowdfund.abi, networkData.address)
+    const Req = await basicCrowdfund.methods.Requirement().call()
+    this.setState({Requirement : Req}) 
+    //Just Trying out Contract Calls Please Check and If necessary delete it!
 
+
+  }
+
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      NeeetID : 22000,
+      NetID : 0,
+      Requirement : null
+    };
   }
    
 
@@ -53,25 +70,29 @@ class App extends Component {
     return (
       <div>
         <div className="App">      
-    <Toolbar/>   
+    <Toolbar account={this.state.account}/>   
+    
+    <button  className="Host_Button" >Host</button>
     
  
       
-
+    
+   
     
     
 
     <form>
       <div className="ProjList">
 
-      <label >Contract Name is "{basic_Crowdfund.contractName}"<br></br><br></br><br></br></label>  
-      <label >Fundraising {Annex} 2 <br></br><br></br><br></br></label>  
-      <label >Fundraising {Annex} 3 <br></br><br></br><br></br></label>  
-      <label >Fundraising {Annex} 4 <br></br><br></br><br></br></label>  
+       
+      <label >Contract Name : "{basic_Crowdfund.contractName}"<br></br><br></br><br></br></label>  
+      <label >The Network ID : {this.state.NetID} <br></br><br></br><br></br></label>  
+      <label >'Requirement' State : {this.state.Requirement}  <br></br><br></br><br></br></label>  
+      <label >Fundraising {Annex} List ID <br></br><br></br><br></br></label>  
 
       </div>
     </form> 
-   
+    
     
                     </div>
       </div>
--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,7 +1,10 @@
 import './toolbar.css';
 import React from 'react';
 import Logo from '../Logo.png';
+import Identicon from 'identicon.js';
 
+
+// var account = "0x64efC29cC6E3c5663e54C48A290A9321371C88a2"; //hardcoded please change => Update Changed
 
 
 const toobar = props => {
@@ -9,11 +12,19 @@ const toobar = props => {
                 <header className='HeadTool'>
                 <nav>
                 <div className='toolbar'>
-                       
+                
                        <img src={Logo} alt="Logo" width="420"/>
                        
+                        <div className="AccountAlign">
+                         <large >{props.account}</large>     
+                              
+                        </div>
+
+                       
+                       
+
                 </div>
-                               
+               
                </nav>
                
                <div className="Paragraph">
